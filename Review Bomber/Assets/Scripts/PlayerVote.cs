0f20ee12f7c0335422d67d55db65ad990b988e15
kris <kriskuerten@gmail.com>
2026-02-23 16:32:51 -0800

    g

--- Review Bomber/Assets/Scripts/PlayerVote.cs
+++ Review Bomber/Assets/Scripts/PlayerVote.cs
@@ -1,11 +1,13 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class PlayerVote : MonoBehaviour
 {
     public string assignedPlayer;
     public bool hasVoted;
     [SerializeField] private GameObject votedImage;
+    [SerializeField] private TMP_Text playerNameText;
     [SerializeField] private Color notVotedColor;
     [SerializeField] private Color yesVotedColor;
 
